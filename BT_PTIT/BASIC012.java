d3e23a385bd4b7809e5026f20186c919393e2ede
DooxFonggg <phongcoi1235@gmail.com>
2024-02-24 03:35:08 +0700

    BASIC012 - Số lượng các chữ số tăng dần lớn nhất trong dãy

--- BT_PTIT/BASIC012.java
+++ BT_PTIT/BASIC012.java
@@ -2,44 +2,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Nhom03_oop24;
+package BT_PTIT;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  *
  * @author phong
  */
-public class test {
-  public static void main(String[] args) {
+public class BASIC012 {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         input.nextLine();
-        while (t > 0) {
+        while(t > 0) {
             ArrayList<Integer> arr = new ArrayList<>();
-            String line = input.nextLine();
-            String[] numbers = line.split("\\s+");
-            for (String number : numbers) {
-                    arr.add(Integer.parseInt(number));
-            }
+            String line = input.nextLine(); // nhap vao thành string "1 2 3 4 5"
+            String[] numbers = line.split("\\s+"); // loại bỏ những dấ cách khi nhập mảng
+            for(String x : numbers) {
+                    arr.add(Integer.parseInt(x)); // chuyển về số xong add
+                }
             System.out.println(longestSortedSequence(arr));
             t--;
-            // Đọc dòng mới để tiếp tục
         }
     }
-
+    
     public static int longestSortedSequence(ArrayList<Integer> arr) {
         int max = 0;
         int currentMax = 1;
-        if (arr.size() == 0) {
+        if(arr.size() == 0) {
             return 0;
         }
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr.get(i) >= arr.get(i - 1)) {
+        for(int i = 1; i<arr.size(); i++) {
+            if(arr.get(i) >= arr.get(i-1)) {
                 currentMax++;
-            } else {
+            }
+            else {
                 max = Math.max(max, currentMax);
                 currentMax = 1;
             }
