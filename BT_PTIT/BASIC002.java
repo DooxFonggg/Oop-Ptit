000c756041770fae4965d25b1df74449aa0d2675
DooxFonggg <phongcoi1235@gmail.com>
2024-04-04 02:04:24 +0700

    BASIC002 - Sử dụng Scanner viết chuỗi cho trước

--- BT_PTIT/BASIC002.java
+++ BT_PTIT/BASIC002.java
@@ -1,8 +1,10 @@
+package BT_PTIT;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package BT_PTIT;
+
 
 /**
  *
