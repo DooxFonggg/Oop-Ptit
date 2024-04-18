0481e354adc963413502927226c545427a15cc29
DooxFonggg <phongcoi1235@gmail.com>
2024-04-18 13:55:49 +0700

    OOP002 - class Rectangle

--- BT_PTIT/OOP002.java
+++ BT_PTIT/OOP002.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Nhom03_oop24;
+package BT_PTIT;
 
 import java.util.Scanner;
 
