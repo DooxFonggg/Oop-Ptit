614da4eed1876a5a2746536b7cd7b91cd58553e0
DooxFonggg <phongcoi1235@gmail.com>
2024-05-04 06:32:29 +0700

    STRING004 - Sử dụng StringBuffer để lấy chiều dài chuỗi

---
+++ BT_PTIT/STRING004.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BT_PTIT;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author phong
+ */
+public class STRING004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        input.nextLine();
+        while(t-- > 0) {
+            String str = input.next();
+            StringBuffer s = new StringBuffer(str);
+            System.out.println(s.length());
+        }
+        input.close();
+    }
+}
